288aae965ea8d995242e1aa689a5fb0e163c6538
kenier <kenier202@gmail.com>
2024-12-05 11:23:30 -0500

    feat(signals): add routing

--- src/app/products/signals/signals-routing.module.ts
+++ src/app/products/signals/signals-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignalesLayoutComponent } from './layout/signales-layout.component';
+import { CounterPageComponent } from './pages/counter-page/counter-page.component';
+import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: SignalesLayoutComponent,
     children: [
       {
-        path: '',
-        component: SignalesLayoutComponent,
+        path: 'counter',
+        component: CounterPageComponent
+      },
+      {
+        path: 'user-info',
+        component: UserInfoPageComponent,
+      },
+      {
+        path: 'properties',
+        component: PropertiesPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'counter',
       }
     ]
   }
